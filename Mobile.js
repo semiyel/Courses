d5d26fa1368e37060e5825c6cb4b2b747b61862d
semiyel <semiyelshvets@gmail.com>
2023-01-27 21:17:04 +0200

    add: Mobile touch practick

---
+++ Mobile.js
@@ -0,0 +1,33 @@
+// touchstart
+// touchmove
+// tuchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
+
+  box.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+
+    console.log("start");
+    console.log(e.targetTouches);
+  });
+
+  box.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+
+    console.log(e.targetTouches[0].pageX);
+  });
+
+  //   box.addEventListener("touchend", (e) => {
+  //     e.preventDefault();
+
+  //     console.log("start");
+  //   });
+  // Властивості
+  // touches
+  // targetTouches
+  // changedTouches
+});
